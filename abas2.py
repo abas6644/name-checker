e33f1904313d2cbb0c1cc5c3172aef94e47040bb
Abasiubong <ubongabasi09@gmail.com>
2021-12-09 09:51:26 -0800

    second commit

---
+++ abas2.py
@@ -0,0 +1,6 @@
+usernames=['admin','bola','joel','sammy','eddy']
+name = input("your username ")
+if name=='admin':
+    print("hello admin,would you like to see a status report?") 
+else:
+        print("thank you for logging in again") 
